7d3547344f285154ef5324c504d60ef07a08d72d
Peter Todorov <peter.todorov@live.com>
2024-10-27 11:00:51 +0000

    Rename variables

--- microphone_real_time.py
+++ microphone_real_time.py
@@ -5,6 +5,7 @@ from matplotlib.animation import FuncAnimation
 import matplotlib.gridspec as gridspec
 from collections import deque
 
+
 # Audio stream parameters
 AUDIO_FORMAT = pyaudio.paInt16  # Audio format (16-bit integer)
 AUDIO_CHANNELS = 1              # Number of audio channels (mono)
@@ -34,7 +35,7 @@ ax3 = fig.add_subplot(gs[:, 1])
 x_time = np.arange(0, AUDIO_BUFFER)
 
 # Create an initial empty line for the time-domain plot
-line_time, = ax1.plot(x_time, np.zeros(AUDIO_BUFFER))
+plot_time_data, = ax1.plot(x_time, np.zeros(AUDIO_BUFFER))
 
 # Set the y-axis limits for time-domain plot to 16-bit PCM range
 ax1.set_ylim(-3000, 3000)
@@ -47,7 +48,7 @@ ax1.set_ylabel("Amplitude")
 x_freq = np.fft.fftfreq(AUDIO_BUFFER, 1/SAMPLING_RATE)[:AUDIO_BUFFER//2]  # Frequency axis (only positive frequencies)
 
 # Create an initial empty line for the frequency-domain plot
-line_freq, = ax2.plot(x_freq, np.zeros(AUDIO_BUFFER//2))
+plot_frequency_data, = ax2.plot(x_freq, np.zeros(AUDIO_BUFFER//2))
 
 # Set the y-axis to log scale for the frequency-domain plot (FFT magnitude)
 ax2.set_yscale("log")
@@ -61,7 +62,7 @@ ax2.set_ylabel("Magnitude")
 
 # Set occupancy
 ax3.set_title("Occupancy")
-occupancy_count_text = ax3.text(0.5, 0.5, "", fontsize=200, ha="center", va="center")
+plot_occupancy_count = ax3.text(0.5, 0.5, "", fontsize=200, ha="center", va="center")
 ax3.axis("off")
 occupancy_filter = deque(maxlen=5)
 
@@ -80,7 +81,7 @@ def update_plots(frame):
     data = np.frombuffer(stream.read(AUDIO_BUFFER, exception_on_overflow=False), dtype=np.int16)
     
     # Update the time-domain line data
-    line_time.set_ydata(data)
+    plot_time_data.set_ydata(data)
     
     # Compute the FFT of the audio data and take the absolute magnitude
     fft_data = np.abs(np.fft.fft(data))[:AUDIO_BUFFER//2]  # Keep only the positive frequencies
@@ -90,15 +91,15 @@ def update_plots(frame):
     fft_data_smoothed = moving_average(fft_data, window_size=9)
     
     # Update the frequency-domain line data with the smoothed data
-    line_freq.set_ydata(fft_data_smoothed)
+    plot_frequency_data.set_ydata(fft_data_smoothed)
     occupancy_count = 1 if np.mean(np.std(data)) > 20 else 0
     occupancy_count = addElementAndGetMedian(occupancy_filter, occupancy_count)
-    occupancy_count_text.set_text(occupancy_count)
+    plot_occupancy_count.set_text(occupancy_count)
     
-    return line_time, line_freq, occupancy_count_text
+    return plot_time_data, plot_frequency_data, plot_occupancy_count
 
 # Create a real-time animation using FuncAnimation
-ani = FuncAnimation(fig, update_plots, blit=True, interval=30)
+animation = FuncAnimation(fig, update_plots, blit=True, interval=30)
 
 # Show the plot in a blocking way so it keeps updating in real-time
 plt.tight_layout()
