af6affa5e8e6fab839454beabeb81f091c8c487d
Peter Todorov <peter.todorov@live.com>
2024-11-05 18:37:39 +0000

    Add 'Seconds-from-start'

--- count_people_for_data_gathering_ui.py
+++ count_people_for_data_gathering_ui.py
@@ -9,20 +9,30 @@ RECORDING_NAME = input("Recording_name: ").replace(" ", "_")
 FOLDER = "data_gathering"
 os.makedirs(FOLDER, exist_ok=True)
 csv_file = f"{FOLDER}/{TIME_STAMP}_{RECORDING_NAME}.csv"
+first_time = None
 
 with open(csv_file, mode="w", newline="") as file:
     writer = csv.writer(file)
-    writer.writerow(["Time", "Seconds", "Count"])
+    writer.writerow(["Time", "Seconds", "Seconds-from-start", "Count"])
 print("File created:")
 print(csv_file)
 
-def show_number(number):
+def show_number(occupancy_count):
     current_time = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
     time_in_seconds = time.time()
+    global first_time
+    if first_time is None:
+        first_time = time_in_seconds
+    seconds_from_start = time_in_seconds - first_time
     with open(csv_file, mode="a", newline="") as file:
         writer = csv.writer(file)
-        writer.writerow([current_time, time_in_seconds, number])
-    label.config(text=f"{number}")
+        writer.writerow([
+            current_time,
+            round(time_in_seconds, 2),
+            round(seconds_from_start, 2),
+            occupancy_count,
+        ])
+    label.config(text=f"{occupancy_count}")
 
 # Initialize the main application window
 root = tk.Tk()
